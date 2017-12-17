a43d27eba56eaa984a10ae4041c0fad379992f06
Justin Rahardjo <justindra91@gmail.com>
2017-12-17 23:11:15 +1000

    feat(nvm): Install NVM

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 wget -O xt  http://git.io/v3DBV && chmod +x xt && ./xt && rm xt
 
 # Git
-
 apt install git
 ln -sfv ~/.dotfiles/git/.gitconfig ~
+
+# NVM (https://github.com/creationix/nvm)
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash

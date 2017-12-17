aa3dd67aac0ec65141dd651e989857e766fece75
Justin Rahardjo <justindra91@gmail.com>
2017-12-17 23:03:09 +1000

    feat(terminal): Set terminal colors

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
+# Terminal
+# Set colors (https://github.com/Mayccoll/Gogh/blob/master/content/themes.md)
+wget -O xt  http://git.io/v3DBV && chmod +x xt && ./xt && rm xt
+
 # Git
 
 apt install git

9943a0775dec11b13de91acb6de0b5e173837437
Justin Rahardjo <justindra91@gmail.com>
2017-12-17 23:27:33 +1000

    feat(install): Add checks to see if things has been installed yet or not

--- install.sh
+++ install.sh
@@ -3,8 +3,16 @@
 wget -O xt  http://git.io/v3DBV && chmod +x xt && ./xt && rm xt
 
 # Git
-apt install git
-ln -sfv ~/.dotfiles/git/.gitconfig ~
+if test ! $(which git); then
+    apt install git
+    ln -sfv ~/.dotfiles/git/.gitconfig ~
+else
+    echo "git already installed..."
+fi
 
 # NVM (https://github.com/creationix/nvm)
-wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+if [ "$(command -v nvm)" = "nvm" ]; then
+    wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+else
+    echo "nvm already installed..."
+fi

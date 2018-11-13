9c38c3d840e947e38dc7e194a2e7d521ee047cb3
Justin Rahardjo <justindra91@gmail.com>
2018-11-13 14:50:39 -0500

    Updated to have NVM and node

--- init/setup.sh
+++ init/setup.sh
@@ -36,3 +36,22 @@ case "$(uname -s)" in
 		;;
 esac
 
+if [ -f "$HOME/.bashrc" ] && [ ! -h "$HOME/.bashrc" ]
+then
+    echo "[i] Move current ~/.bashrc to ~/bashrc_backup"
+    mv "$HOME/.bashrc" "$HOME/bashrc_backup"
+fi
+
+if [ -f "$HOME/.bash_profile" ] && [ ! -h "$HOME/.bash_profile" ]
+then
+    echo "[i] Move current ~/.bash_profile to ~/bash_profile_backup"
+    mv "$HOME/.bash_profile" "$HOME/bash_profile_backup"
+fi
+
+# Run main playbook
+echo "[i] Run Playbook"
+ansible-playbook ../ansible/dotfiles.yml --ask-become-pass
+
+echo "[i] From now on you can use $ dotfiles to manage your dotfiles"
+echo "[i] Done."
+

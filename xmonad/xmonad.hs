a603cadb540f1324a3ad972c7db6d8a01a78b830
Justin Rahardjo <justindra91@gmail.com>
2021-03-11 21:28:52 -0800

    feat: add lock screen

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -153,6 +153,8 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     --
     -- , ((modm              , xK_b     ), sendMessage ToggleStruts)
 
+    -- Lock xmonad
+    , ((modm .|. shiftMask, xK_l     ), spawn "xscreensaver-command -lock")
     -- Quit xmonad
     , ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
 
@@ -170,7 +172,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     --
     [((m .|. modm, k), windows $ f i)
         | (i, k) <- zip (XMonad.workspaces conf) ([xK_1 .. xK_9] ++ [xK_0])
-        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
     ++
     --
     -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
